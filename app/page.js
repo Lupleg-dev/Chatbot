59ef706e116e553f97ed1711cec44975f98283be
marksikaundi <emmanuelmark117@gmail.com>
2023-12-18 07:43:28 +0200

    Add page component to app

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1 className='text-black '>Academy still in Development</h1>
+    </div>
+  )
+}
+
+export default page
